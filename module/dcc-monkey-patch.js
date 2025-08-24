64980d17e65969eece9f8a31644fb71e9fbfd342
Blaze Sanecki <sanecki.blazej@gmail.com>
2025-08-24 20:24:46 +0200

    Replaces title field with actor field

--- module/dcc-monkey-patch.js
+++ module/dcc-monkey-patch.js
@@ -118,6 +118,22 @@ class DCCMonkeyPatch {
                 }
             }
         }
+
+        // Replace Title field with actor field
+        let originalOnRender = DCCActorSheet.prototype._onRender;
+        DCCActorSheet.prototype._onRender = function (app, html, data) {
+            originalOnRender.call(this, app, html, data);
+            // Replace the title field with the actor's name if it's a XCC sheet
+            if(app.actor.system.class.localizationPath) {
+                console.log("Patching title field to actor name", app,html,data,this);
+                let element = this.parts.character.firstElementChild.querySelector('label[for="system.details.title.value"]');
+                element.textContent = game.i18n.localize('XCC.Actor');
+                element.for='system.details.casting';
+                element = this.parts.character.firstElementChild.querySelector('input[name="system.details.title.value"]');
+                element.id = element.name = 'system.details.casting';
+                element.value = app.actor.system.details?.casting || 'Movie Star';
+            }
+        };
     }
 }
 
